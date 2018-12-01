35a1336a5ef5362f72aaa4c0b4f8bee9c716af71
Kai Beckmann <kai.beckmann@hs-rm.de>
2018-12-01 12:08:44 +0100

    option to deactivate IPV6 Privacy extension

--- 1_Pi_Config.sh
+++ 1_Pi_Config.sh
@@ -160,6 +160,15 @@ if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then
 	dpkg-reconfigure tzdata
 fi
 
+echo -n "Do you want to use permanent IPv6 EUIs? [y/N] "
+read
+if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+    echo "Deactivating IPv6 Privacy Extensions"
+    touch /etc/sysctl.d/10-ipv6-privacy.conf
+    replaceAppend /etc/sysctl.d/10-ipv6-privacy.conf "^.*net.ipv6.conf.all.use_tempaddr=.*$" "net.ipv6.conf.all.use_tempaddr=0"
+    replaceAppend /etc/sysctl.d/10-ipv6-privacy.conf "^.*net.ipv6.conf.default.use_tempaddr=.*$" "net.ipv6.conf.default.use_tempaddr=0"
+fi
+
 if [[ ! -f /usr/local/bin/log2ram ]]; then
 	echo -n "Do you want to enable log2ram [y/N] "
 	read
